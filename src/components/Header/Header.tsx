95bbafb1bdf85e8685c6d117ac74b3a235728680
Karina Barinova <kbarinova11@gmail.com>
2024-03-15 20:53:33 -0400

    Add header with navbar

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,20 @@
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+
+export const Header = () => {
+  return (
+    <header>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Employee Management System</Navbar.Brand>
+          <Navbar.Toggle />
+          <Navbar.Collapse className="justify-content-end">
+            <Navbar.Text>
+              Add a new user
+            </Navbar.Text>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
